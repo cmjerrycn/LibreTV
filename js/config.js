1bb4e529172a6727a074e3699383e03562ecaf9f
cmjerrycn <58454044+cmjerrycn@users.noreply.github.com>
2025-05-09 14:47:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -38,7 +38,7 @@ const API_SITES = {
     ckzy: {
         api: 'https://www.ckzy1.com',
         name: 'CK资源',
-        adult: true
+        //adult: true
     },
     zy360: {
         api: 'https://360zy.com',
@@ -80,27 +80,27 @@ const API_SITES = {
     jkun: {
         api: 'https://jkunzyapi.com',
         name: 'jkun资源',
-        adult: true
+        //adult: true
     },
     bwzy: {
         api: 'https://api.bwzym3u8.com',
         name: '百万资源',
-        adult: true
+        //adult: true
     },
     souav: {
         api: 'https://api.souavzy.vip',
         name: 'souav资源',
-        adult: true
+        //adult: true
     },
     r155: {
         api: 'https://155api.com',
         name: '155资源',
-        adult: true
+        //adult: true
     },
     lsb: {
         api: 'https://apilsbzy1.com',
         name: 'lsb资源',
-        adult: true
+        //adult: true
     },
     huangcang: {
         api: 'https://hsckzy.vip',
@@ -115,7 +115,7 @@ const API_SITES = {
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源',
-        adult: true
+        //adult: true
     },
     yinghua: {
         api: 'https://m3u8.apiyhzy.com',
